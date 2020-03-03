3933bed6f3f229f420834fc0e951e65b5a6633fe
Egor.Fyodorov <1426200@mail.ru>
2020-03-03 17:21:50 +0500

    1233

--- zarplati/zarplati/Form1.cs
+++ zarplati/zarplati/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace zarplati
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=303-16; Initial Catalog=Zarplati; Integrated Security=true;");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand($"Select Login_manager From manager where Login_manager = '{textBox1.Text}' and Password = '{textBox2.Text}'", con);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+                MessageBox.Show("заходи дружище");
+            else
+                MessageBox.Show("ты не наш");
         }
     }
 }
